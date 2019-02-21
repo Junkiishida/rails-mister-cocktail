943a66bd0ae9bef7d40187e42f65c1cea13ec228
Junki Ishida <junky5092@gmail.com>
2019-02-21 21:40:07 +0900

    updated files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Ouzo")
+Ingredient.create(name: "Raki")
